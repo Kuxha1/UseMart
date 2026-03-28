5dc2bd072d068770115a8ae8225fbcf6ac45b435
Kushal Mistry <kpmistry104@gmail.com>
2026-03-28 19:06:45 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,28 @@
+/*
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydb";
+*/
+
+/*
+// Use this coded for Local MySQL Database
 <?php
-$conn = mysqli_connect("127.0.0.1:3307", "root", "", "usemart");
+$conn = new mysqli($servername,$username,$password,$dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+*/
+
+// Use this coded phpmyadmin
+/*
+<?php
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+*/
